70e048d1a6dd3c13d66d76b0310faf4126fd1236
Juanca <juancasan17@gmail.com>
2019-11-06 08:33:20 -0600

    Initial Commit

---
+++ betquin/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  betquin
+//
+//  Created by Juanca Sánchez on 11/6/19.
+//  Copyright © 2019 Juanca Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ betquin/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  betquin
+//
+//  Created by Juanca Sánchez on 11/6/19.
+//  Copyright © 2019 Juanca Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
